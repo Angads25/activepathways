dd04255bd9b4519eb397c06b772af4fe5b02bd83
fakruddin <fakru@codalien.com>
2018-02-21 18:35:58 +0530

    changes some ui in landing page and dashboard page

--- service-web/src/components/header/header.js
+++ service-web/src/components/header/header.js
@@ -2,6 +2,12 @@ export default {
   name: 'Header',
   data () {
     return {
+      type:0
+    }
+  },
+  computed : {
+    showHeader(){
+      return this.$route.name !== "dashboard"
     }
   }
 }
