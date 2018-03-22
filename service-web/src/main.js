60e501bea231270aa360b2a8f8073f9ef7cf932f
fakruddin <fakru@codalien.com>
2018-03-22 19:07:08 +0530

    change calendar dashboard page and add all modal input text transition

--- service-web/src/main.js
+++ service-web/src/main.js
@@ -7,8 +7,11 @@ import router from './router'
 import store from './store'
 import './mixins'
 import 'sweetalert2/dist/sweetalert2.css'
+import VCalendar from 'v-calendar'
+import 'v-calendar/lib/v-calendar.min.css'
 
 Vue.use(VeeValidate)
+Vue.use(VCalendar)
 
 // CSS library
 require('../node_modules/tachyons/css/tachyons.css')
--- service-web/src/views/Dashboard/dashboard.js
+++ service-web/src/views/Dashboard/dashboard.js
@@ -1,11 +1,38 @@
-import Datepicker from 'vuejs-datepicker'
+// import Datepicker from 'vuejs-datepicker'
 export default {
   name: 'Dashboard',
   components: {
-    Datepicker
   },
   data () {
     return {
+      selectedDate: null,
+      themeStyles: {
+        wrapper: {
+          border: '1'
+        },
+        header: {
+          color: '#fafafa',
+          backgroundColor: '#f142f4',
+          borderColor: '#404c59',
+          borderWidth: '1px 1px 0 1px'
+        },
+        headerVerticalDivider: {
+          borderLeft: '1px solid #404c59'
+        },
+        weekdays: {
+          color: '#ffffff',
+          backgroundColor: '#f142f4',
+          borderColor: '#ff0098',
+          borderWidth: '0 1px',
+          padding: '5px 0 10px 0'
+        },
+        weekdaysVerticalDivider: {
+          borderLeft: '1px solid #404c59'
+        },
+        weeks: {
+          border: '1px solid #dadada'
+        }
+      }
     }
   }
 }
