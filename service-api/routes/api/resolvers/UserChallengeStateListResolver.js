18cfe95e9b5f94ec208c88b637d4ee3df054da6c
Anurag Patel <anuragpatel10@gmail.com>
2018-03-23 18:20:34 +0530

    find insted of findone

--- service-api/routes/api/resolvers/UserChallengeStateListResolver.js
+++ service-api/routes/api/resolvers/UserChallengeStateListResolver.js
@@ -11,7 +11,7 @@ module.exports = {
 				let query = {};
 				if (!request._user && !request._user._id) return reject(new Error('Permission denied!'));
 				query.user = request._user._id;
-				UserChallengeState.findOne(query).exec((err, _programme) => {
+				UserChallengeState.find(query).exec((err, _programme) => {
 					if (err) return reject(err);
 					if (_programme) return resolve(_programme);
 					resolve({})
