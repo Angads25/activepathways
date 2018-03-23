06ae3b7f5367451a8b3c7e98c389da1b579b27fe
Anurag Patel <anuragpatel10@gmail.com>
2018-03-23 18:04:50 +0530

    no acitivity found

--- service-web/src/views/Dashboard/dashboard.js
+++ service-web/src/views/Dashboard/dashboard.js
@@ -48,5 +48,6 @@ export default {
   created () {
     this.$store.dispatch('fetchUserChallengeStateList')
     this.userChallengeStateList = this.$store.userChallengeStateList
+    console.log(this.userChallengeStateList , "asdasdhasdhjsadhlasjdhklashdjasdhl")
   }
 }
