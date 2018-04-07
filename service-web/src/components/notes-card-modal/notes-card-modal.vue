b5fad54f9ae3a1796cbaf62c741b6ed679ee77e7
fakruddin <fakru@codalien.com>
2018-04-07 19:15:48 +0530

    fixed some trello bugs

---
+++ service-web/src/components/notes-card-modal/notes-card-modal.vue
@@ -0,0 +1,5 @@
+<template src="./notes-card-modal.html"></template>
+
+<script src="./notes-card-modal.js"></script>
+
+<style src="./notes-card-modal.css" scoped></style>
--- service-web/src/views/LandingPage/landing-page.js
+++ service-web/src/views/LandingPage/landing-page.js
@@ -1,3 +1,6 @@
+import SignIn from '@/components/login/signIn/signIn.vue'
+import SignUp from '@/components/login/signUp/signUp.vue'
+import ForgetPassword from '@/components/login/forgetPassword/forgetPassword.vue'
 export default {
   name: 'LandingPage',
   data () {
@@ -7,7 +10,41 @@ export default {
           id: 'back-top-title',
           label: 'BackTop'
         }
-      ]
+      ],
+      activeModal: '',
+      isOpen: false,
+      openerText: 'Open'
+    }
+  },
+  components: {
+    SignIn,
+    SignUp,
+    ForgetPassword
+  },
+  methods: {
+    closeModal () {
+      document.body.classList.remove('modal-open')
+      this.activeModal = ''
+    },
+    openModal () {
+      document.body.classList.add('modal-open')
+    },
+    open () {
+      this.openerText = 'Close'
+      this.isOpen = true
+      document.body.classList.add('overlay-bg')
+    },
+    close () {
+      this.openerText = 'Open'
+      this.isOpen = false
+      document.body.classList.remove('overlay-bg')
+    },
+    toggle () {
+      if (this.isOpen) {
+        this.close()
+      } else {
+        this.open()
+      }
     }
   }
 }
