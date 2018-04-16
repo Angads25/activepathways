d9b4e8c584305047646759160b5645956044f37f
meghna1711 <meghnagogna111@gmail.com>
2018-04-16 23:29:46 +0530

    added jobs for challenge start and challenge end

---
+++ service-api/jobs/DailyCheckinJob.js
@@ -0,0 +1,57 @@
+const EmailService = require("../services/EmailService"),
+	UserChallengeState = require('keystone').list('UserChallengeState').model,
+	async = require('async'),
+	moment = require('moment'),
+	ObjectId = require('mongoose').Types.ObjectId;
+
+module.exports = class DailyCheckinJob {
+
+	static get trigger() {
+		return "0 8 * * *"; // 8:00 AM every day
+	}
+
+	static task(err, done) {
+		console.log("Triggering", this.name);
+		let userChallenges, tasks = [];
+		const greaterThanDate = moment().startOf('day')._d;
+		const lessThanDate = moment().endOf('day')._d;
+
+		tasks.push((callback) => {
+			UserChallengeState.find(
+				{
+					challengeDate: {
+						$gte: greaterThanDate,
+						$lt: lessThanDate
+					},
+					status: "PENDING"
+				}
+			).populate("user").exec((err, _challenges) => {
+				if (err) return callback(err);
+				else callback(null, userChallenges = _challenges || [])
+			})
+		});
+
+		tasks.push((callback) => {
+			async.mapLimit(userChallenges, 10, (_challenge, callback) => {
+				EmailService.sendMail(_challenge.user.email, "You have a new challenge!", "emailNewChallenge", {
+					username: _challenge.user.name.first,
+					websiteUrl: process.env.WEBSITE_URL,
+					challengeId: _challenge._id
+				}, (err, emailResp) => {
+					console.log("Sending Mail", _challenge.user);
+					if (err) console.log("email error", err);
+					callback(err, emailResp);
+				})
+			}, (err, done) => {
+				callback(err, done);
+			})
+		});
+
+		async.series(tasks, (err, res) => {
+			console.log("Job Completed");
+			if (err) console.log("err ", this.name, err);
+			done(err, res);
+		})
+	}
+}
+    
