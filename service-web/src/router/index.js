3180e01f69c54b9b39aa409923b8a67934f669ae
Sonu Malik <iamsonumalik@gmail.com>
2018-04-20 18:15:37 +0530

    redirecting to home if not logged in

--- service-web/src/router/index.js
+++ service-web/src/router/index.js
@@ -4,9 +4,10 @@ import LandingPage from '@/views/LandingPage/landing-page.vue'
 import Dashboard from '@/views/Dashboard/dashboard.vue'
 import ChallengeStates from '@/views/ChallengeStates/challenge-states.vue'
 import ChallengePage from '@/views/challenge-page/challenge-page.vue'
+import { LocalData } from '../services/localData'
 
 Vue.use(Router)
-export default new Router({
+const router =  new Router({
   routes: [
     {
       path: '/dashboard',
@@ -24,9 +25,30 @@ export default new Router({
       component: LandingPage
     },
     {
+      path: '/',
+      name: 'home-view',
+      component: LandingPage
+    },
+    {
       path: '/challenge/:id',
       name: 'challenge-detail',
       component: ChallengePage
     }
   ]
 })
+router.beforeEach((to, from, next) => {
+  let authCheck = LocalData.fetch.authToken()
+  if (['dashboard', 'challengestates', 'challenge-detail'].indexOf(to.name) > -1) {
+    if (authCheck) {
+      next()
+    } else {
+      next({
+        name: 'home-view'
+      })
+    }
+  }else{
+    next()
+  }
+})
+
+export default router
