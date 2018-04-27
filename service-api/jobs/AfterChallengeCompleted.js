70669945d74c3f085878e12f6b35dd5a12a4f518
Anurag Patel <anuragpatel10@gmail.com>
2018-04-27 12:15:24 +0530

    Add end of the programme email template

---
+++ service-api/jobs/AfterChallengeCompleted.js
@@ -0,0 +1,43 @@
+const EmailService = require("../services/EmailService"),
+	UserChallengeState = require('keystone').list('UserChallengeState').model,
+	async = require('async'),
+	moment = require('moment');
+
+module.exports = class DailyCheckoutJob {
+
+	static get trigger() {
+		return "0 9 * * *"; // 9:00 AM every day
+	}
+
+	static task(err, done) {
+		console.log("Triggering", this.name);
+		let userChallenges, tasks = [];
+		const greaterThanDate = moment().subtract(1,'days').startOf('day')._d;
+		const lessThanDate = moment().subtract(1,'days').endOf('day')._d;
+
+		tasks.push((callback) => {
+			UserChallengeState.find({challengeDate: {$gte: greaterThanDate,$lt: lessThanDate},status: 'COMPLETED'}).populate("user").exec((err, _challenges) => {
+				if (err) return callback(err);
+				else callback(null, userChallenges = _challenges || [])
+			})
+		});
+		
+		tasks.push((callback) => {
+			async.mapLimit(userChallenges, 10, (_challenge, callback) => {
+				EmailService.sendMail(_challenge.user.email, "How did it go Today? Please check-in", "programmeEmail", {
+					username: _challenge.user.name.first
+				}, (err, emailResp) => {
+					if (err) console.log("email error", err);
+					callback(err, emailResp);
+				})
+			}, (err, done) => {
+				callback(err, done);
+			})
+		});
+
+		async.series(tasks, (err, res) => {
+			if (err) console.log("err ", this.name, err);
+			done(err, res);
+		})
+	}
+}
