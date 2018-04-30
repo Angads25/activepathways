7d2354ec8a17debd75238a461a47acd122604d42
Anurag Patel <anuragpatel10@gmail.com>
2018-04-30 14:51:52 +0530

    fix after challenge completed job

--- service-api/jobs/AfterChallengeCompleted.js
+++ service-api/jobs/AfterChallengeCompleted.js
@@ -1,9 +1,10 @@
 const EmailService = require("../services/EmailService"),
 	UserChallengeState = require('keystone').list('UserChallengeState').model,
+	AppUser = require('keystone').list('AppUser').model,
 	async = require('async'),
 	moment = require('moment');
 
-module.exports = class DailyCheckoutJob {
+module.exports = class AfterChallengeCompleted {
 
 	static get trigger() {
 		return "0 9 * * *"; // 9:00 AM every day
@@ -12,24 +13,42 @@ module.exports = class DailyCheckoutJob {
 	static task(err, done) {
 		console.log("Triggering", this.name);
 		let userChallenges, tasks = [];
-		const greaterThanDate = moment().subtract(1,'days').startOf('day')._d;
-		const lessThanDate = moment().subtract(1,'days').endOf('day')._d;
+		const greaterThanDate = moment().subtract(1, 'days').startOf('day')._d;
+		const lessThanDate = moment().subtract(1, 'days').endOf('day')._d;
 
 		tasks.push((callback) => {
-			UserChallengeState.find({challengeDate: {$gte: greaterThanDate,$lt: lessThanDate},status: 'COMPLETED'}).populate("user").exec((err, _challenges) => {
+			UserChallengeState.aggregate([{
+				$match: {
+					challengeDate: {$gte: greaterThanDate, $lt: lessThanDate},
+					status: {$in: ['COMPLETED', 'SKIPPED']}
+				}
+			}, {
+				$group: {
+					_id: "$programme",
+					user: {$first: '$user'},
+					count: {$sum: 1}
+				},
+			}, {
+				$match: {"count": {$gt: 6}}
+			}]).exec((err, _challenges) => {
 				if (err) return callback(err);
 				else callback(null, userChallenges = _challenges || [])
 			})
 		});
-		
+
 		tasks.push((callback) => {
-			async.mapLimit(userChallenges, 10, (_challenge, callback) => {
-				EmailService.sendMail(_challenge.user.email, "How did it go Today? Please check-in", "programmeEmail", {
-					username: _challenge.user.name.first
-				}, (err, emailResp) => {
-					if (err) console.log("email error", err);
-					callback(err, emailResp);
-				})
+			async.mapLimit(userChallenges, 10, (obj, callback) => {
+				AppUser.findOne({_id: obj.user}).exec((err, _user) => {
+					if (err) return callback(err);
+					else {
+						EmailService.sendMail(_user.email, "Please fill the short survey ", "programmeEmail", {
+							username: _user.name.first
+						}, (err, emailResp) => {
+							if (err) console.log("email error", err);
+							callback(err, emailResp);
+						})
+					}
+				});
 			}, (err, done) => {
 				callback(err, done);
 			})
