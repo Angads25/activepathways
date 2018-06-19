0e8b3ac99c9289fa009b3aa3b4dc5de437aab9df
fakruddin <fakru@codalien.com>
2018-06-19 13:06:08 +0530

    developing program page

---
+++ service-web/src/components/programHeader/programHeader.vue
@@ -0,0 +1,10 @@
+<template src="./programHeader.html">
+
+</template>
+
+<script src="./programHeader.js">
+</script>
+
+<style src="./programHeader.css" scoped>
+
+</style>
---
+++ service-web/src/views/ProgramPage/programPage.js
@@ -0,0 +1,17 @@
+import ProgramHeader from '@/components/programHeader/programHeader.vue'
+export default {
+  name: 'ProgramCatalogue',
+  data() {
+    return {
+      options: [
+        {
+          id: 'back-top-title',
+          label: 'BackTop'
+        }
+      ]
+    }
+  },
+  components: {
+    ProgramHeader
+  }
+}
